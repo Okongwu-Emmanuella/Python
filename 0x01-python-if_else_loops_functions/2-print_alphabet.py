62f888cff7b58ecd006ace29b198676ca6784b5b
Emmanuella <emmanuellaokongwu@gmail.com>
2024-04-14 18:00:46 +0100

    Python practice files

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,16 @@
+"""
+Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
+
+You can only use one print function with string format
+You can only use one loop in your code
+You are not allowed to store characters in a variable
+You are not allowed to import any module
+"""
+# for chr(range(''))
+
+for num in range(97, 123):
+
+    print(chr(num), end="")
+
+
+
